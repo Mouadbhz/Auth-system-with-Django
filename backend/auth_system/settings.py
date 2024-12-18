87d489cb80cd0d16b3f950a524f09da2388dec90
Mouad Benhizia <101869459+Mouadbhz@users.noreply.github.com>
2024-12-18 17:27:24 +0100

    Update settings.py

--- backend/auth_system/settings.py
+++ backend/auth_system/settings.py
@@ -91,7 +91,13 @@ DATABASES = {
     }
 }
 #EMAIL ( you find this lines in the Read Me.txt ) 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST ='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sellerspark2@gmail.com'
+EMAIL_HOST_PASSWORD = 'qpdmnrpgedsfvxuq' # the second one 
+# EMAIL_HOST_PASSWORD = 'rsjpywuuoebwlmrl'
+EMAIL_USE_TLS = True  
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
